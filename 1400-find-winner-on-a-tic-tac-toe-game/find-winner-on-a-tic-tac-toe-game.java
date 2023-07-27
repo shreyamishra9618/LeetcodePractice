23f4b0c78388565bc6b24323eb2469cd6b25f9b6
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2023-07-27 16:13:05 -0700

    Time: 0 ms (100.00%) | Memory: 40.8 MB (6.75%) - LeetSync

--- 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.java
+++ 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.java
@@ -1,60 +1,69 @@
-// class Solution {
-//     public String tictactoe(int[][] moves) {
-        
-//     }
-// }
-
 class Solution {
     public String tictactoe(int[][] moves) {
-        // even add 1; odd add -1;
-        int[] rows = new int[3];
-        int[] cols = new int[3];
-        int diagonal = 0;
-        int antidiagonal = 0;
-        for(int i = 0; i < moves.length; i++) {
-            int add = i % 2 == 0 ? 1 : - 1;
-            int x = moves[i][0];
-            int y = moves[i][1];
-            
-            rows[x] += add;
-            cols[y] += add;
-            
-            if(x == y) {
-                diagonal += add;
-            }
-            
-            // anti-diangonal, col = n - row - 1;这是考点；
-            if(y == 3 - x - 1) {
-                antidiagonal += add;
+        int n = moves.length;
+        char[][] board = new char[3][3];
+        for(int i =0; i < 3 ;i ++){
+            for(int j =0 ; j < 3 ; j ++){
+                board[i][j] = '.';
             }
         }
-        if(isAwin(rows, cols, diagonal, antidiagonal)) {
-            return "A";
+
+        for(int i =0; i < n ; i++){
+            if(i % 2 == 0){
+                board[moves[i][0]][moves[i][1]] = 'X';
+            }else{
+                board[moves[i][0]][moves[i][1]] = '0';  
+            }
         }
-        if(isBwin(rows, cols, diagonal, antidiagonal)) {
-            return "B";
+
+        return checkBoard(board);
+    }
+
+    private String checkBoard( char[][] board){
+        // check all columns 
+        for(int j =0 ; j < 3 ; j++){
+            int countA =0;
+            int countB =0;
+            for(int i =0; i < 3; i++){
+                if(board[i][j] == 'X') countA++;
+                else if(board[i][j] == '0') countB++;
+            }
+            if(countA == 3) return "A";
+            if(countB == 3) return "B";
         }
-        if(moves.length == 9) {
+            // check all rows 
+        for(int j =0 ; j < 3 ; j++){
+            int countA =0;
+            int countB =0;
+            for(int i =0; i < 3; i++){
+                if(board[j][i] == 'X') countA++;
+                else if(board[j][i] == '0') countB++;
+            }
+            if(countA == 3) return "A";
+            if(countB == 3) return "B";
+        }
+        // check diognal
+        if(board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') return "A";
+        if(board[0][0] == '0' && board[1][1] == '0' && board[2][2] == '0') return "B";
+        // check anti diognal
+        if(board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') return "A";
+        if(board[0][2] == '0' && board[1][1] == '0' && board[2][0] == '0') return "B";
+        // check for Draw/ pending conditions
+        int c =0;
+        for(int i = 0; i < 3 ; i++){
+            for( int j =0; j < 3 ; j++){
+                if(board[i][j] == 'X' || board[i][j] == '0'){
+                    c++;
+                }
+            }
+        }
+        if(c != 9){
+            return "Pending";
+        }
+        else{
             return "Draw";
         }
-        return "Pending";
-    }
-    
-    private boolean isAwin(int[] rows, int[] cols, int diagonal, int antidiagonal) {
-        for(int i = 0; i < 3; i++) {
-            if(rows[i] == 3 || cols[i] == 3) {
-                return true;
-            }
-        }
-        return diagonal == 3 || antidiagonal == 3;
-    }
-    
-    private boolean isBwin(int[] rows, int[] cols, int diagonal, int antidiagonal) {
-        for(int i = 0; i < 3; i++) {
-            if(rows[i] == -3 || cols[i] == -3) {
-                return true;
-            }
-        }
-        return diagonal == -3 || antidiagonal == -3;
+
+
     }
 }
