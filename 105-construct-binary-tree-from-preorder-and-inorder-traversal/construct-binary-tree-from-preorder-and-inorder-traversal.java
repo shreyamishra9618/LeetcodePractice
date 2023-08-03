3ddf60381b188237724bd16657023c7823d70eaf
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2023-08-03 14:20:58 -0700

    Time: 3 ms (44.92%) | Memory: 43.5 MB (39.65%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,26 +15,27 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if( preorder == null || inorder == null || preorder.length != inorder.length){
+        if(preorder == null || inorder == null || preorder.length != inorder.length){
             return null;
         }
-        return helperTree( preorder, inorder, 0 ,0 , inorder.length -1);
+         return helperTree(preorder, inorder, 0,0, inorder.length -1); 
     }
 
-private TreeNode helperTree(int[] preorder, int[] inorder, int preStart, int inStart, int inEnd){
-    if(preStart > preorder.length -1 || inStart > inEnd) return null;
-    int rootVal = preorder[preStart];
-    TreeNode root = new TreeNode(rootVal);
-    int inIndex = 0;
-    for(int i =inStart; i <= inEnd ; i++){
-        if( inorder[i]  == root.val){
-            inIndex =i;
-            break;
-        }
+ private TreeNode helperTree(int[] preorder, int[] inorder, int preStart, int inStart, int inEnd){
+         if(preStart > preorder.length -1 || inStart > inEnd){
+             return null;
+         }
+         int rootVal = preorder[preStart];
+         TreeNode root = new TreeNode(rootVal);
+         int rootindex = 0;
+         for(int i =inStart; i <= inEnd ; i++){
+            if(inorder[i] == root.val){
+                rootindex =i;
+                break;
+            }
+         }
+         root.left = helperTree(preorder, inorder, preStart +1, inStart, rootindex - 1);
+        root.right = helperTree(preorder, inorder, preStart + rootindex - inStart +1, rootindex +1, inEnd);
+        return root;
     }
-    int leftSubtree = inIndex - inStart;
-    root.left = helperTree( preorder , inorder, preStart +1, inStart ,inIndex - 1 );
-    root.right =helperTree( preorder, inorder, preStart + leftSubtree +1, inIndex + 1 ,inEnd);
-    return root;
-}
 }
