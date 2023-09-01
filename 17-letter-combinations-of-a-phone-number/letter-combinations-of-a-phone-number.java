542fca5619836be84a7953e87b063280713b0e76
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2023-08-31 23:28:29 -0700

    Time: 5 ms (48.93%) | Memory: 41.4 MB (39.44%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,24 +1,18 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         LinkedList<String> ans = new LinkedList<>();
-        String[] mapping = new String[]{"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         if(digits.isEmpty()) return ans;
         ans.add("");
+        String[] mapping = new String[]{"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         for(int i =0; i < digits.length(); i++){
             int x = Character.getNumericValue(digits.charAt(i));
-            System.out.print("i - ");
-            System.out.println(i);
             while(ans.peek().length() == i){
-               String t = ans.remove();
-               System.out.print("t - ");
-               System.out.println(t);
-               for(char s : mapping[x].toCharArray()){
-                System.out.print("t + s - ");
-                 System.out.println(t + s);
-                   ans.add(t + s);
-               }
+                String t = ans.remove();
+                for(char s : mapping[x].toCharArray()){
+                    ans.add(t + s);
+                }
             }
         }
-        return ans;
+       return ans; 
     }
 }
