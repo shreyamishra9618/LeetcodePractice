906b6a5c9535349cbccb755923a0274e2107cbf0
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2023-09-06 14:58:15 -0700

    Time: 10 ms (82.52%) | Memory: 45 MB (74.96%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,40 +1,37 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int sum =0;
-        int max = 0;
-        int n = weights.length;
-        for(int i =0; i < n; i++){
-            max = Math.max(max, weights[i]);
-            sum += weights[i];
-        }
-        int low = max;
-        int high = sum;
-        int ans =0;
-
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if(func(mid, weights, n , days)){
-                ans =mid;
-                high = mid -1;
-            }
-            else{
-                low = mid +1;
-            }
-        }
-        return ans;
+      int sum =0;
+      int max = 0;
+      int n = weights.length;
+      for(int i =0; i < n ; i++){
+          max = Math.max(max, weights[i]);
+          sum += weights[i];
+      } 
+      int low = max;
+      int high = sum;
+      int ans =0;
+      while(low <= high){
+          int mid = low + (high - low)/2;
+          if(func(mid, weights, n, days)){
+              ans = mid;
+              high = mid -1;
+          }
+          else low = mid +1;
+      }
+      return ans; 
     }
-    public boolean func( int mid, int[] weights, int n, int days){
+    public boolean func(int mid, int[] weights, int n , int days){
+        int noOfDays =1;
         int sum =0;
-        int day =1;
-        for(int i =0; i < n; i ++){
+        for(int i =0; i < n; i++){
             if(sum + weights[i] <= mid) sum += weights[i];
             else{
-                sum = weights[i];
-                day++;
-
+                sum = weights[i];// resetting the sum value for next iteration
+                noOfDays++;
             }
         }
-        if( day <= days ) return true;
+        if(noOfDays <= days) return true;
         else return false;
     }
+
 }
