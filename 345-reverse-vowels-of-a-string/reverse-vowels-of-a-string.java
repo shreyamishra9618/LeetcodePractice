efd92e47d2b8a2cb6d839a06f8632ef83dcf4a05
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2023-09-07 16:02:21 -0700

    Time: 3 ms (83.66%) | Memory: 43.7 MB (73.24%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,30 +1,26 @@
 class Solution {
     public String reverseVowels(String s) {
-        
-        char[] strs = s.toCharArray();
-        int left =0;
-        int right = s.length()-1;
-        while(left < right){
-         while(left < right && !isVowel(strs[left])){
-           left++;
-         }
-          while(left < right && !isVowel(strs[right])){
-           right--;
-        }
-        if(left < right){
-            char temp = strs[left];
-            strs[left] = strs[right];
-            strs[right] = temp;
-            left++;
-            right--;
-        }
-        
+      char[] strs = s.toCharArray();
+      int left =0;
+      int right = s.length()-1;
+      while(left < right){
+          while(left < right && !isVowel(strs[left])){
+              left++;
+          }
+           while(left < right && !isVowel(strs[right])){
+              right--;
+          }
+          if(left < right){
+              char temp = strs[left];
+              strs[left] = strs[right];
+              strs[right] = temp;
+              left++;
+              right--;
+          }
+      }
+      return new String(strs);
     }
-    return new String(strs);
-    
-}
-public boolean isVowel(char c){
+    public boolean isVowel(char c){
         return "aeiouAEIOU".indexOf(c) != -1;
-        
     }
 }
