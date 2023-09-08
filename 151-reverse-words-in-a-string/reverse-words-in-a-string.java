06c041cc951c12061fb6ffe703066a06b8baf109
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2023-09-08 11:40:11 -0700

    Time: 8 ms (63.10%) | Memory: 42 MB (79.48%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,27 +1,18 @@
 class Solution {
     public String reverseWords(String s) {
-     String[] words = s.trim().split("\\s+");
-     int len = words.length;
-
-        // Iterate over the words and do something with each word
-        int low = 0;
-        int high =len -1;
+        String[] words = s.trim().split("\\s+");
+        int len = words.length;
+        int low =0;
+        int high = len -1;
         while(low < high){
-            int mid = low + (high -low)/2;
             String temp = words[low];
             words[low] = words[high];
             words[high] = temp;
             low++;
             high--;
         }
-
-        for (int i = 0; i < words.length; i++) {
-    System.out.print(words[i]);
-}
-
-String concatenatedString = String.join(" ", words);
-
-return concatenatedString;
-
+        String ans = String.join(" ", words);
+        return ans;
+        
     }
 }
