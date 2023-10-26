6928333d6f72dc1274f0652c53695c4fcdd9d89c
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2023-10-26 06:48:23 -0700

    Time: 6 ms (92.06%) | Memory: 41.6 MB (24.89%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        if(s.length()==1 || s.length() == 0){
+            return true;
+        }
+       int [] mapS = new int[256];
+        int [] mapT = new int[256];
+
+        for(int i =0; i< s.length(); i++)
+           {
+            if(mapS[s.charAt(i)]!=mapT[t.charAt(i)])
+                return false;
+
+            mapS[s.charAt(i)]=i+1;
+            mapT[t.charAt(i)]=i+1;
+        }
+        return true;
+    }
+}
