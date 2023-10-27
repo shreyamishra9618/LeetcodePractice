a026c284a508dbf9d1432e56fe64c6c09d9c2976
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2023-10-27 09:05:34 -0700

    Time: 0 ms (100.00%) | Memory: 39.8 MB (61.33%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,10 +15,23 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        Stack<TreeNode> stackp = new Stack<>();
+        Stack<TreeNode> stackq = new Stack<>();
         if( p == null && q == null) return true;
-        if(p == null || q == null ) return false;
-        if(p.val != q.val) return false;
-        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
-        
+        if(p == null || q == null) return false;
+        if( p != null) stackp.push(p);
+        if( q != null) stackq.push(q);
+        while(!stackp.isEmpty() && !stackq.isEmpty()){
+            TreeNode newp = stackp.pop();
+            TreeNode newq = stackq.pop();
+            if(newp.val != newq.val) return false;
+            if(newp.right != null) stackp.push(newp.right);
+             if(newq.right != null) stackq.push(newq.right);
+             if(stackp.size() != stackq.size()) return false;
+            if(newp.left != null) stackp.push(newp.left);
+             if(newq.left != null) stackq.push(newq.left);
+               if(stackp.size() != stackq.size()) return false;
+        }
+       return stackp.size() == stackq.size(); 
     }
 }
