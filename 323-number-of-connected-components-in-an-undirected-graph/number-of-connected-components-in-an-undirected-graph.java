0d1ceaf826b4f2067c2b810cdaec1dc0ec4b57fc
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2023-11-02 16:47:09 -0700

    Time: 4 ms (58.03%) | Memory: 44.2 MB (25.84%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -2,25 +2,25 @@ class Solution {
     public int countComponents(int n, int[][] edges) {
         List<List<Integer>> adjList = new ArrayList<>();
         int[] visited = new int[n];
-        for(int i  =0 ; i < n ; i++){
+        int comp =0;
+        for(int i =0; i < n ; i++){
             adjList.add(new ArrayList<>());
         }
         for(int[] edge : edges){
             adjList.get(edge[0]).add(edge[1]);
             adjList.get(edge[1]).add(edge[0]);
         }
-        int comp =0;
-        for(int i =0; i < n ; i++){
-            if(visited[i]==0){
+        for(int i =0; i < n; i++){
+            if(visited[i] ==0){
                 comp++;
                 dfs(adjList, visited, i);
             }
         }
         return comp;
     }
-    private void dfs( List<List<Integer>> adjList,  int[] visited, int startIndx){
-        visited[startIndx] = 1;
-        for(int nei : adjList.get(startIndx)){
+    private void dfs( List<List<Integer>> adjList,int[] visited, int idx){
+        visited[idx] =1;
+        for(int nei : adjList.get(idx)){
             if(visited[nei] == 0){
                 dfs(adjList, visited, nei);
             }
