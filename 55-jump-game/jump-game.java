8f5cbdd4358dbaca06149819175f1f743dbb6b83
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2023-11-26 10:47:40 -0800

    Time: 1 ms (99.98%) | Memory: 44.2 MB (59.89%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int goodLastSteps = nums.length-1;
-        for(int i = nums.length-1; i >= 0; i--){
-            if(i + nums[i] >= goodLastSteps){
-                goodLastSteps =i;
+        int lastPos = nums.length -1;
+        for(int i = nums.length -1; i >= 0; i--){
+            if(i + nums[i] >= lastPos){
+                lastPos =i;
             }
         }
-        return goodLastSteps == 0;
+        return lastPos ==0;
     }
 }
