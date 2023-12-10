94ac28544c86e25f8563dfea056ce8e423cd3eaf
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2023-12-10 10:01:46 -0800

    Time: 3 ms (5.56%) | Memory: 42.9 MB (57.41%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,11 +1,16 @@
 class Solution {
+    HashMap<Integer, Integer> memo = new HashMap<>();
     public int minCostClimbingStairs(int[] cost) {
-        int [] minStepsCost = new int[cost.length +1];
-       for(int i =2; i < minStepsCost.length; i++){
-           int takeOneStep = minStepsCost[i-1] + cost[i-1];
-           int takeTwoSteps = minStepsCost[i-2]+cost[i-2];
-           minStepsCost[i]= Math.min(takeOneStep, takeTwoSteps);
-       }
-       return minStepsCost[minStepsCost.length -1];
+        return minCost(cost.length, cost);   
+    }
+    public int minCost(int i, int[] cost){
+        if( i <= 1) return 0;
+        if(memo.containsKey(i)){
+            return memo.get(i);
+        }
+        int takeOneStep = minCost(i-1, cost)+ cost[i-1];
+        int takeTwoStep = minCost(i - 2, cost) + cost[i-2];
+        memo.put(i, Math.min(takeOneStep, takeTwoStep) );
+        return memo.get(i);
     }
 }
