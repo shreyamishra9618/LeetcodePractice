92deae993e185a343562bec673bf629b077dcdfc
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2023-12-11 16:35:05 -0800

    Time: 8 ms (87.91%) | Memory: 57.5 MB (34.06%) - LeetSync

--- 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
+++ 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
@@ -1,23 +1,16 @@
 class Solution {
-    private int [] nums, multipliers;
-    private int n , m;
-    private int[][] memo;
+    // Bottom up solution
     public int maximumScore(int[] nums, int[] multipliers) {
-        
-        this.nums = nums;
-        this.multipliers = multipliers;
-        n = nums.length;
-        m = multipliers.length;
-        this.memo = new int[m][m];
-        return dp(0,0);
-    }
-    public int dp(int i, int left){
-        if(i == m) return 0;
-        int mult = multipliers[i];
-        int right = n -1 -(i - left);
-        if(memo[i][left] == 0){
-            memo[i][left] = Math.max(mult * nums[left] + dp(i +1, left +1), mult * nums[right] + dp(i+1, left) );
+        int n = nums.length;
+        int m = multipliers.length;
+        int[][] memo = new int[m+1][m+1];
+        for(int i = m -1; i >=0; i--){
+            for(int left =i; left >= 0; left --){
+                int right = n -1 - ( i -left);
+                int mult = multipliers[i];
+                memo[i][left] = Math.max(mult * nums[left]+ memo[i+1][left+1], mult * nums[right] + memo[i+1][left]);
+            }
         }
-        return memo[i][left];
+        return memo[0][0];
     }
 }
