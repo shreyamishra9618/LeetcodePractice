f5593ea7cc7abdacb6c0eb750ce3e438969455de
Shreya Mishra <74151853+shreyamishra9618@users.noreply.github.com>
2024-01-23 21:22:42 -0800

    Time: 27 ms (97.46%) | Memory: 44.5 MB (51.65%) - LeetSync

---
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -0,0 +1,21 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        Node p1 = p;
+        Node p2 = q;
+        while(p1 != p2){
+            p1 = p1 == null ? q : p1.parent;
+            p2 = p2 == null ? p : p2.parent;
+        }
+       return p1; 
+    }
+}
